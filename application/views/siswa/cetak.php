ac25b69fb11d4b3936783b681e175484559a6596
n8sense <dionoid1805@gmail.com>
2023-05-20 21:57:26 +0800

    first commit

---
+++ application/views/siswa/cetak.php
@@ -0,0 +1,82 @@
+<?php
+
+
+?>
+
+<html>
+
+<head>
+    <title>Pembayaran SPP SMAN 5 Banjarbaru</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Pembayaran SPP SMAN 5 Banjarbaru</h2>
+        <div class="data-tables datatable-dark">
+
+            <!-- Masukkan table nya disini, dimulai dari tag TABLE -->
+            <table id="mauexport" class="table table-bordered">
+
+
+                <thead>
+                    <tr>
+                        <th scope="col">Nama Siswa</th>
+                        <th scope="col">NIK</th>
+                        <th scope="col">Bulan Bayar</th>
+                        <th scope="col">Jumlah Bayar (Rp)</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Sisa</th>
+                        <th scope="col">Nama Petugas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($transaksi as $row) : ?>
+                        <tr>
+                            <td><?php echo $row['nama_siswa']; ?></td>
+                            <td><?php echo $row['nik']; ?></td>
+                            <td><?php echo $row['bulan_bayar']; ?></td>
+                            <td><?php echo $row['jmlh_bayar']; ?></td>
+                            <td><?php echo $row['status']; ?></td>
+                            <td><?php echo $row['sisa']; ?></td>
+                            <td><?php echo $row['nama_petugas']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+
+            </table>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#mauexport').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+
+                ]
+            });
+        });
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
+</body>
+
+</html>
